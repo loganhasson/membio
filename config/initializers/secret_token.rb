36b52a5ba86be178288e738fa73019e40edaa050
Logan Hasson <logan.hasson@gmail.com>
2013-11-28 13:25:29 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Membio::Application.config.secret_key_base = 'ee288f1c119815ff909359480b4ca8ed6a6137d5f186b1d59c2a4466b60eddbaf09d3d4da604b7a198ae760b5c009c0920f99be8ddf2c87f1cc6d00ca3e06b7d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Membio::Application.config.session_store :cookie_store, key: '_membio_session'
