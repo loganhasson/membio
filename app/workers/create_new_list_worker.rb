52285094a9ec5116f88a61e72270ef39aedc250a
Logan Hasson <logan.hasson@gmail.com>
2013-11-28 23:40:21 -0500

    Add support for creating and adding to lists at the same time

--- app/workers/create_new_list_worker.rb
+++ app/workers/create_new_list_worker.rb
@@ -3,7 +3,14 @@ class CreateNewListWorker
   sidekiq_options retry: false
 
   def perform(phone_number, body)
-    list_title = body[1..-1].downcase
+
+    if body.split('+').size > 1
+      add = true
+      list_title = body.split('+').first[1..-1].downcase
+    else
+      add = false
+      list_title = body[1..-1].downcase
+    end
 
     list = User.find_by(phone_number: phone_number).lists.build(title: list_title)
     if list.save
@@ -15,6 +22,10 @@ class CreateNewListWorker
     end
     
     TextMessage.new(phone_number).create_new_list(list_title, message, success).deliver
+
+    if add
+      AddToListWorker.perform_async(phone_number, body[1..-1])
+    end
   end
 
 end
