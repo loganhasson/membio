b3f61dd6ae921b3990d0efefa30ca454ba557cf8
Logan Hasson <logan.hasson@gmail.com>
2013-11-30 14:38:57 -0500

    Make faye more secure

--- app/workers/feed_broadcast_worker.rb
+++ app/workers/feed_broadcast_worker.rb
@@ -4,7 +4,7 @@ class FeedBroadcastWorker
 
   def perform(phone_number, body)
     combined_data = {phone_number: phone_number, body: body}.to_json
-    message = {:channel => '/getting-started/feed', :data => combined_data}
+    message = {:channel => '/getting-started/feed', :data => combined_data, :ext => {:auth_token => ENV['FAYE_TOKEN']}}
     uri = URI.parse("http://localhost:9292/getting-started/faye")
     Net::HTTP.post_form(uri, :message => message.to_json)
   end
