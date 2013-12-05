005fbc9c42da1b5a94aa50f36c4bba33417ffbd1
Logan Hasson <logan.hasson@gmail.com>
2013-12-04 21:36:12 -0500

    Alter item sql statement for processing delayed reminders

--- app/workers/schedule_reminder_worker.rb
+++ app/workers/schedule_reminder_worker.rb
@@ -14,7 +14,7 @@ class ScheduleReminderWorker
       list_exists = true
     elsif user
       item_ids = user.lists.map {|list| list.id}.flatten
-      if item = Item.find_by(:content => list_title_or_item).where('list_id IN ?', item_ids)
+      if item = Item.where('list_id IN ?', item_ids).where(:content => list_title_or_item)
         item_exists = true
       end
     else
